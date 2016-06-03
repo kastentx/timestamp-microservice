0e645596a61d15a0e29d3bb56006e432a27d3c65
Nick Kasten <kastentx@gmail.com>
2016-06-03 08:20:12 +0000

    fourth commit. input/output functions complete

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ var timeStamp = require('./timestamp.js')
 var express = require('express');
 var app = express();
 var portNum = process.env.PORT || 5000;
-var stringMe = "Hello String!";
 
 /* serves main page */
 app.get('/', function (req, res) {
@@ -19,7 +18,6 @@ app.get('/', function (req, res) {
 app.get('/:inputString', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify(timeStamp.dateType(req.param('inputString'))));
-  //res.send(dateCheck(req.param('inputString')));
 })
 .listen(portNum, function() {
   console.log("Listening on " + portNum);  
--- timestamp.js
+++ timestamp.js
@@ -1,18 +1,31 @@
 var strftime = require('strftime')
 var exports = module.exports = {}
 
-exports.dateType = function(myString) {
-  if (/^\d*$/g.test(myString)) {
-    return unixOutput(myString)
+exports.dateType = function(userInput) {
+  if (/^\d*$/g.test(userInput)) {
+    return unixOutput(userInput)
+  } else if (/[a-zA-Z]+\s\d{1,2}[,]?\s\d{1,4}/.test(userInput)){
+    return naturalOutput(userInput)
   } else {
-    return 'Not an Unix Epoch.'  
-    // nonUnixOutput 
+      return nullOutput()
   }
 }
 
 var unixOutput = function(epoch) {
-    var naturalDate = strftime('%B %e' + ', ' + '%Y', new Date(parseInt(epoch)))
-    
-  return { unix: epoch
+  var unixEpoch = epoch * 1000
+  var naturalDate = strftime('%B %e' + ', ' + '%Y', new Date(unixEpoch))
+  return { unix: parseInt(epoch)
          , natural: naturalDate }
+}
+
+var naturalOutput = function(date) {
+  var unixEpoch = strftime('%s', new Date(date))
+  var naturalDate = strftime('%B %e' + ', ' + '%Y', new Date(date))
+  return { unix: parseInt(unixEpoch)
+         , natural: naturalDate }
+}
+
+var nullOutput = function () {
+  return { unix: null
+         , natural: null }    
 }
